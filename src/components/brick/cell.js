958f574635314a8d963397271db063e1cc2e73f3
zhenglipeng <1252012805@qq.com>
2018-11-08 20:04:52 +0800

    开始处理执行逻辑

--- src/components/brick/cell.js
+++ src/components/brick/cell.js
@@ -2,25 +2,68 @@ import { uuid } from 'lib/common';
 
 export const CLOCK = Symbol('系统时钟');
 
+/**
+ * Cell
+ * id     组件id
+ * type   组件类型(COMPONENT, FUNCTION)
+ * name   组件名称
+ * input  输入组件引用数组
+ * body   组件体, type = COMPONENT 时 body为Cell实例数组; type = FUNCTION 时 body 为 function
+ */
 export default class Cell {
   constructor(config) {
     this.clock = 0;
 
-    if (config.from) {
-      const root = config.components[config.from];
-      this.body = root.links.map(d => {
-        const c = config.components[d.component];
-        if (c.type === 'COMPONENT') {
-          return new Cell({...config, from: d.component});
-        }
-        return new Cell({
-          ...d,
-          ...c
-        })
-      });
-      return;
+    if (config.from) {  //完整配置
+      this.initByFullConfig(config);
+    } else {            //简单配置
+      this.initBySimpleConfig(config);
     }
+  }
 
+  initByFullConfig(config) {
+    const root = config.components[config.from];
+
+    this.id = config.id || uuid();
+    this.type = root.type;
+    this.name = root.name;
+    this.input = config.input || [];
+
+    console.log('========', root.name, root.output)
+
+    this.body = root.links.map(d => {
+      const c = config.components[d.component];
+      if (c.type === 'COMPONENT') {
+        const cMap = c.input.reduce((p, n, i) => {
+          p[n] = d.input[i];
+          return p;
+        }, {});
+        return new Cell({
+          components: {
+            ...config.components,
+            [d.component]: {
+              ...c,
+              input: d.input,
+              links: c.links.map(l => ({
+                ...l,
+                input: l.input.map((key, i) => cMap[key] || key)
+              }))
+            }
+          },
+          id: d.id,
+          input: d.input,
+          output: c.output,
+          from: d.component
+        });
+      }
+      return new Cell({
+        ...d,
+        ...c
+      })
+    });
+  }
+
+  initBySimpleConfig(config) {
     this.id = config.id || uuid();
     this.type = config.type;
     this.name = config.name;
