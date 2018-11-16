eae0eab87365a52a28d3d53cc79959a3f3d4bf05
zhenglipeng <1252012805@qq.com>
2018-11-16 23:26:35 +0800

    开始接入编辑器

--- src/pages/project/detail.js
+++ src/pages/project/detail.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { DragSource, DropTarget } from 'lib/dnd';
+
 import './detail.scss';
 
 export default class extends Component {
@@ -24,6 +26,19 @@ export default class extends Component {
     });
   };
 
+  handleDrop = (data, e) => {
+    const boundingRect = e.target.getBoundingClientRect();
+    const x = e.pageX - boundingRect.x;
+    const y = e.pageY - boundingRect.y;
+
+    console.log('===', x, y)
+    //if (data.type === TYPE_VIEW) {
+    //  this.modules.push(new View({x, y}));
+    //} else {
+    //  this.modules.push(new M({x, y}));
+    //}
+  };
+
   render() {
     const { project } = this.state;
     if (!project) {
@@ -40,10 +55,44 @@ export default class extends Component {
         </nav>
         <div className="body">
           <div className="sidebar">
-            sidebar
+            <div>
+              <h2>基础组件</h2>
+              {DragSource('FUNCTION', {})(
+                <div className="brick-card infinity-hover">
+                  ƒ(x)
+                </div>
+              )}
+            </div>
+            <div>
+              <h2>自定义组件</h2>
+              {['AAA', 'BBB', 'CCC'].map(d => DragSource('COMPONENT', {})(
+                <div className="brick-card infinity-hover">
+                  {d}
+                </div>
+              ))}
+            </div>
+            <div>
+              <h2>公共组件</h2>
+              {['111', '222', '333'].map(d => DragSource('COMPONENT', {})(
+                <div className="brick-card infinity-hover">
+                  {d}
+                </div>
+              ))}
+            </div>
           </div>
           <div className="content">
-            content
+            {DropTarget(['FUNCTION', 'COMPONENT'], {
+              onDrop: this.handleDrop
+            })(
+              <svg>
+                <g className="lines" ref="lines">
+
+                </g>
+                <g className="modules">
+
+                </g>
+              </svg>
+            )}
           </div>
         </div>
       </div>
